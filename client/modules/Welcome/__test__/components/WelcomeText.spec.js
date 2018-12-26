5267766f9c8670c32fe9c45b2ee6f0645ac34ef7
tord <tord.standnes@gmail.com>
2018-12-26 22:08:26 +0100

    added tests to welcome text component

--- client/modules/Welcome/__test__/components/WelcomeText.spec.js
+++ client/modules/Welcome/__test__/components/WelcomeText.spec.js
@@ -1,25 +1,18 @@
-/*
 import React from 'react';
 import test from 'ava';
 import { shallow } from 'enzyme';
 import WelcomeText from '../../components/WelcomeText';
 
+const wrapper = shallow(<WelcomeText />);
 
-test('header has class', t => {
-  const wrapper = shallow(
-    <WelcomeText />
-  );
-
-  t.true(wrapper.hasClass('welcome-header'));
+test('that component renders', t => {
+  t.true(wrapper.find('div').children.length >= 1);
 });
 
-
-test('text has class', t => {
-  const wrapper = shallow(
-    <WelcomeText />
-  );
-
-  t.true(wrapper.hasClass('welcome-text'));
+test('component has a headline', t => {
+  t.is(wrapper.find('h1').length, 1);
 });
-*/
 
+test('component has at least one paragraph', t => {
+  t.true(wrapper.find('p').length >= 1);
+});
