9e61e597e52ae9c9683dc4fc183ef5f76c63c0cb
tord <tord.standnes@gmail.com>
2018-12-29 20:41:25 +0100

    added a test file without tests and fiksed css grid bug

--- client/modules/App/__tests__/App.spec.js
+++ client/modules/App/__tests__/App.spec.js
@@ -2,7 +2,6 @@ import React from 'react';
 import test from 'ava';
 import { shallow } from 'enzyme';
 import { App } from '../App';
-import styles from '../App.css';
 
 const children = <h1>test</h1>;
 const wrapper = shallow(<App children={children} />);
@@ -13,10 +12,6 @@ test('render properly', t => {
 
 test('prop children renders', t => {
   t.true(wrapper.contains(children));
-});
-
-test('proper styling/ styling imported', t => {
-  t.truthy(wrapper.find('div > div').hasClass(styles.container));
 });
 
 test.todo('responsive design');
--- client/modules/Welcome/components/WelcomeText.js
+++ client/modules/Welcome/components/WelcomeText.js
@@ -9,12 +9,13 @@ function WelcomeText() {
       <h1 className={styles['welcome-header']}>
         Welcome
       </h1>
+      <br />
       <p className={styles['welcome-text']}>
         My name is Tord Standnes and this my personal web page.<br />
         For now the only use of this website is for me to play around with.<br />
         Feel free to look around. <br />
         <br />
-        This web page is written with the MERN stack.
+        This application is written with the MERN stack.
       </p>
     </div>
   );
